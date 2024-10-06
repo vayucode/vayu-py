005c391d43525616783f513614cc5dad1e707689
Fadi Atamny <fadi@withvayu.com>
2024-10-06 16:13:40 +0300

    fixed again few issues with structure of auto refresher

--- vayu_sdk/clients/vayu_client.py
+++ vayu_sdk/clients/vayu_client.py
@@ -1,11 +1,11 @@
-from datetime import datetime, timedelta
 from openapi.api_client import ApiClient
 from openapi.configuration import Configuration
 from openapi.api.auth_api import AuthApi
 from openapi.models.login_request import LoginRequest
+import jwt
 from datetime import datetime, timedelta
-from requests.adapters import HTTPAdapter
-from requests.sessions import Session
+from openapi.rest import RESTClientObject
+
 
 class VayuClient:
     __access_token: str = None
@@ -16,9 +16,13 @@ class VayuClient:
 
     def __init__(self, api_key: str, host: str = None):
         self.__access_token = None
-        self.__host = host  
+        self.__host = host
         self.__api_key = api_key
-    
+
+    @property
+    def access_token(self):
+        return self.__access_token
+
     @property
     def is_logged_in(self):
         return self.__access_token is not None
@@ -26,20 +30,9 @@ class VayuClient:
     @property
     def client(self):
         configuration = Configuration(host=self.__host, api_key=self.__access_token)
-        session = self.__build_session()
-        return ApiClient(
-            configuration,
-            header_name="Authorization",
-            header_value=f"Bearer {self.__access_token}",
-            session=session,
-        )
-
-    def __build_session(self) -> Session:
-        session = Session()
-        adapter = TokenRefreshAdapter(self)
-        session.mount("https://", adapter)
-        return session
-    
+        client = ApiClient(configuration)
+        client.rest_client = AutoTokenRefresherRESTClient(self, configuration)
+        return client
 
     def login(self):
         configuration = Configuration(host=self.__host)
@@ -50,24 +43,39 @@ class VayuClient:
 
         self.__access_token = refresh_response.access_token
 
+        decoded_token = jwt.decode(
+            self.__access_token, options={"verify_signature": False}
+        )
+        self.__expires_at = datetime.fromtimestamp(decoded_token["exp"])
+
     def refresh_token(self):
-        if self.__access_token is None or datetime.now(datetime.UTC) + self.__token_expiry_threshold > self.__expires_at:
+        if (
+            self.__access_token is None
+            or datetime.now() + self.__token_expiry_threshold > self.__expires_at
+        ):
+            
             self.login()
 
     def validate_logged_in(self):
         if not self.is_logged_in:
-            raise RuntimeError("vayu client is not logged in. please login before calling this method")
+            raise RuntimeError(
+                "Vayu client is not logged in. Please login before calling this method"
+            )
 
-class TokenRefreshAdapter(HTTPAdapter):
-    def __init__(self, client: VayuClient, *args, **kwargs):
-        self.client = client
-        super().__init__(*args, **kwargs)
 
-    def send(self, request, **kwargs):
-        if request.path_url != "/login":
-            self.client.refresh_token()
-            request.headers["Authorization"] = f"Bearer {self.vayu_client._Vayu__access_token}"
-        return super().send(request, **kwargs)
+class AutoTokenRefresherRESTClient(RESTClientObject):
+    def __init__(self, vayu_client: VayuClient, configuration: Configuration, *args, **kwargs):
+        super().__init__(configuration, *args, **kwargs)
+        self.vayu_client = vayu_client
+
+    def request(self, method, url, headers=None, **kwargs):
+        if "/login" not in url:
+            self.vayu_client.refresh_token()
+            if headers is None:
+                headers = {}
+            headers["Authorization"] = f"Bearer {self.vayu_client.access_token}"
+
+        return super().request(method, url, headers=headers, **kwargs)
+
 
 __all__ = ["VayuClient"]
-
